88c822ef24dd1d4d039c37b683e831e267bf75de
SARAR-creation <sarar19sakib@gmail.com>
2026-05-17 19:48:09 +0600

    Creates JWT tokens during login

--- backend/src/auth/auth.module.ts
+++ backend/src/auth/auth.module.ts
@@ -1,5 +1,31 @@
 import { Module } from '@nestjs/common';
 
+import { TypeOrmModule } from '@nestjs/typeorm';
+
+import { JwtModule } from '@nestjs/jwt';
+
+import { AuthController } from './auth.controller';
+import { AuthService } from './auth.service';
+
+import { User } from '../users/entities/user.entity';
+
+import { JwtStrategy } from './strategies/jwt.strategy';
+
 @Module({
+  imports: [
+    TypeOrmModule.forFeature([User]),
+
+    JwtModule.register({
+      secret: process.env.JWT_SECRET,
+
+      signOptions: {
+        expiresIn: '7d',
+      },
+    }),
+  ],
+
+  controllers: [AuthController],
+
+  providers: [AuthService, JwtStrategy],
 })
 export class AuthModule {}
