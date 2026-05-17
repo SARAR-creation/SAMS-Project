833ab9b6877c3d66c60606dd50ac47dda6fc9c33
SARAR-creation <sarar19sakib@gmail.com>
2026-05-17 22:52:03 +0600

    Fixed token for stu.login

--- backend/src/students/students.controller.ts
+++ backend/src/students/students.controller.ts
@@ -1,4 +1,53 @@
-import { Controller } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Get,
+  Patch,
+  Post,
+  Req,
+  UseGuards,
+} from '@nestjs/common';
 
+import { StudentsService } from './students.service';
+
+import { JwtAuthGuard } from '../auth/guards/jwt-auth.guard';
+
+import { ApplyStudentDto } from './dto/apply-student.dto';
+import { UpdateStudentDto } from './dto/update-student.dto';
+
+@UseGuards(JwtAuthGuard)
 @Controller('students')
-export class StudentsController {}
+export class StudentsController {
+  constructor(
+    private studentsService: StudentsService,
+  ) {}
+
+  @Post('apply')
+  apply(
+    @Req() req,
+    @Body() applyDto: ApplyStudentDto,
+  ) {
+    return this.studentsService.apply(
+      req.user.userId,
+      applyDto,
+    );
+  }
+
+  @Get('me')
+  getMyProfile(@Req() req) {
+    return this.studentsService.getMyProfile(
+      req.user.userId,
+    );
+  }
+
+  @Patch('me')
+  updateMyProfile(
+    @Req() req,
+    @Body() updateDto: UpdateStudentDto,
+  ) {
+    return this.studentsService.updateMyProfile(
+      req.user.userId,
+      updateDto,
+    );
+  }
+}
